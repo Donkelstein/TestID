8645ab9d40196a7a388074e1f2f198a564367adb
Donkelstein <te16.strinnholm@umea.nti.se>
2017-11-06 11:14:20 +0100

    Nu räknar den dinna försök!

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -10,21 +10,28 @@ import javax.swing.*;
 public class TestIDE {
     public static void main(String[] args) {
 
+        /*variabler */
         int s;
         int randomNumber = (int) Math.floor(Math.random() * 100);
         boolean kontroll = false;
+        int försök = 0;
 
         while (!kontroll) {
-
+            försök = försök + 1;
 
             s = Integer.parseInt(JOptionPane.showInputDialog(null, "gissa på ett numer mellan 0 och 100"));
 
+            /*
+            kollar om ditt svar är rätt, samt brytter while loopen om ditt svar är rätt.
+             */
+
             if (s > randomNumber) {
-                JOptionPane.showMessageDialog(null, "för högt, testa igen");
+                JOptionPane.showMessageDialog(null, "För högt, testa igen");
             } else if (s < randomNumber) {
-                JOptionPane.showMessageDialog(null, "lite lågt, testa igen");
+                JOptionPane.showMessageDialog(null, "Lite lågt, testa igen");
             } else if (s == randomNumber) {
-                JOptionPane.showMessageDialog(null, "rätt");
+                JOptionPane.showMessageDialog(null, "Rätt!" + " det tog " + försök + " försök");
+
                 kontroll = true;
             }
         }
